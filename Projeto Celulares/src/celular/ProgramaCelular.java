6e00a3fe7bb81b69a86e5551bcd760808c914d21
Eduardo Alves da Rocha <eduardoa8142@gmail.com>
2020-10-10 09:55:39 -0300

    utilizando da melhor forma os atributos estáticos

--- Projeto Celulares/src/celular/ProgramaCelular.java
+++ Projeto Celulares/src/celular/ProgramaCelular.java
@@ -9,9 +9,6 @@ public class ProgramaCelular {
 
 	public static void main(String[] args) {
 
-		ConexaoComOBanco NovaConexao = new ConexaoComOBanco();
-		Celulares celulares = new Celulares();
-
 		String LerSql = "Select * From Celular;";
 		String InserirSql = "Insert Into Celular(nome) Values ('Ifhone 12');";
 		String AtualizarSql = "Update Celular set nome = 'Iphone 12' where id = '7'";
@@ -21,7 +18,7 @@ public class ProgramaCelular {
 		
 		try {
 			
-			Connection conexao = NovaConexao.criarConexao();
+			Connection conexao = ConexaoComOBanco.criarConexao();
 			
 			PreparedStatement ps = conexao.prepareStatement(LerSql);
 			PreparedStatement ps2 = conexao.prepareStatement(AtualizarSql);
@@ -37,17 +34,17 @@ public class ProgramaCelular {
 				String nome = rs.getString("nome");
 				int id = rs.getInt("id");
 				
-				celulares.getNome().add(nome);
-				celulares.getId().add(id);
+				Celulares.getNome().add(nome);
+				Celulares.getId().add(id);
 				
 			}
 			
 			System.out.println("id | nome ");
 			System.out.println();
 			
-			for (int i = 0; i < celulares.getNome().size(); i++) {
+			for (int i = 0; i < Celulares.getNome().size(); i++) {
 				
-				System.out.println(celulares.getId().get(i) + "    " + celulares.getNome().get(i));
+				System.out.println(Celulares.getId().get(i) + "    " + Celulares.getNome().get(i));
 				
 			}
 			
